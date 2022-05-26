9ea80add79a5984f496dc50426d54b833a6f81b1
Samuel Perez Reyes <corazzy@hotmail.com>
2022-05-26 17:42:14 -0500

    Automata para la sucesion 101

---
+++ src/automatas/actividad.java
@@ -0,0 +1,108 @@
+package automatas;
+import java.util.Scanner;
+ 
+ 
+public class actividad {
+ 
+    static char [] caracteres;
+    boolean aceptacion;
+    int contador;
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        actividad automata = new actividad();
+        
+        System.out.println("Ingrese La cadena");
+ 
+        String cad;
+ 
+        cad = sc.nextLine();
+        caracteres = cad.toCharArray();
+        automata.Inicial();
+        
+        if(automata.aceptacion){
+            System.out.println("\n");
+            System.out.println("La Cadena es Valida");
+        }else{
+            System.out.println("\n");
+            System.out.println("La Cadena es Invalida");
+        }
+    }
+ 
+ 
+ 
+    public void Inicial() {
+        contador = 0; //Declaramos en 0 para empezar las transiciones
+        q0();
+    }
+ 
+ 
+    public void q0() {
+        aceptacion = false;
+        if(contador<caracteres.length){
+            if(caracteres[contador]== '0'){
+                System.out.println("Estado q0");
+                contador++;
+                q0(); 
+ 
+            }else if (caracteres[contador]=='1'){
+                contador++;
+                System.out.println("Estado q0");
+                q1();
+            }
+        }
+    }
+    public void q1() {
+        aceptacion = false;
+        if(contador<caracteres.length){
+            if(caracteres[contador]== '1'){
+                System.out.println("Estado q1");
+                contador++;
+                q1(); 
+ 
+            }else if (caracteres[contador]=='0'){
+                contador++;
+                System.out.println("Estado q1");
+                q2();
+            }
+        }
+    }
+    
+    public void q2() {
+        aceptacion = false;
+        if(contador<caracteres.length){
+            if(caracteres[contador]== '1'){
+                System.out.println("Estado q2");
+                contador++;
+                q3(); 
+ 
+            }else if (caracteres[contador]=='0'){
+                contador++;
+                System.out.println("Estado q2");
+                q2();
+            }
+        }
+    }
+    
+    public void q3() {
+        if(contador<caracteres.length){
+            if(caracteres[contador]== '0'){
+                System.out.println("Estado q3");
+                contador++;
+                q3(); 
+            }
+        }
+        else if(contador==caracteres.length){
+            aceptacion = true;
+            System.out.println("Estado q3 de aceptación"); 
+        }else if(contador<caracteres.length){
+            for(int i=2; i<=caracteres.length;i++){
+                if(caracteres[i]==1){
+                    aceptacion = false;
+                }
+            }           
+        }
+    }
+}
+
+        
